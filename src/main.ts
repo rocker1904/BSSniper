7899813f261fa716f9d56170dcde7b533c652637
Batthew627 <batcraft627@gmail.com>
2023-01-04 22:32:06 +0000

    added ranked only check for snipe playlist

--- src/main.ts
+++ src/main.ts
@@ -206,9 +206,9 @@ export async function percentageOfNMumber1s(cachedPlayer: CachedPlayer): Promise
 }
 
 // Returns a playlist of all songs where player1 has a lower score than player2.
-export async function snipePlaylist(P1: CachedPlayer, P2: CachedPlayer): Promise<Playlist> {
+export async function snipePlaylist(P1: CachedPlayer, P2: CachedPlayer,onlyRanked:boolean): Promise<Playlist> {
 
-    const predicate: ScorePredicate = p1Score => P2.playerScores.some(p2Score => p1Score.leaderboard.id === p2Score.leaderboard.id && p1Score.score.modifiedScore < p2Score.score.modifiedScore);
+    const predicate: ScorePredicate = p1Score => P2.playerScores.some(p2Score => p1Score.leaderboard.id === p2Score.leaderboard.id && p1Score.score.modifiedScore < p2Score.score.modifiedScore&& (!onlyRanked || p1Score.score.pp !== 0));
     const playlistName: PlayerToPlaylistName = () => `Snipe ${P2.player.name}`;
     return playlistByPredicate(P1, predicate, playlistName);
 }
