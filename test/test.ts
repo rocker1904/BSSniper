e62ab452cd1043a42936509937a85ed6b6915fd0
samjellis <11990712+rocker1904@users.noreply.github.com>
2023-01-18 15:47:59 +0000

    Update and fix package deps + remove old files

--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { writePlaylist, playlistOfNumber1s, percentageOfNMumber1s, snipePlaylist, rankingQueuePlaylist, playlistOfTopX, playlistOfNotTopX, playlistOfScoresBelowGivenAccuracy, CachedPlayer, getPlayerData, playlistByPercievedWorstScore, playlistByCombo, playlistOfNumber1sWithinXMonths } from './main';
+import { writePlaylist, playlistOfNumber1s, percentageOfNMumber1s, snipePlaylist, rankingQueuePlaylist, playlistOfTopX, playlistOfNotTopX, playlistOfScoresBelowGivenAccuracy, CachedPlayer, getPlayerData, playlistByPercievedWorstScore, playlistByCombo, playlistOfNumber1sWithinXMonths } from '../src/main';
 
 async function noTopLevelAsyncAwait() {
     const pandita = await getPlayerData('76561198186151129');
     await writePlaylist(await playlistOfNumber1sWithinXMonths(pandita, 5));
     // void writePlaylist(await rankingQueuePlaylist());
     console.log('Done!');
+    process.exit();
 }
 
 void noTopLevelAsyncAwait();
